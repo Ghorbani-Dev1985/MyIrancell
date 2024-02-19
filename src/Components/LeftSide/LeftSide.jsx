6748090c321bf645358e54c7b478ea0f6076da07
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-02-19 19:43:31 +0330

    feat: add animate circle in LeftSide component

--- src/Components/LeftSide/LeftSide.jsx
+++ src/Components/LeftSide/LeftSide.jsx
@@ -1,9 +1,16 @@
-import { AccountCircle, MailOutlineOutlined, WifiOutlined } from "@mui/icons-material";
+import {
+  AccountCircle,
+  MailOutlineOutlined,
+  WifiOutlined,
+} from "@mui/icons-material";
 import React from "react";
-import { CircularProgressbar, CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 import { Link } from "react-router-dom";
-import Logo from '../../assets/Images/Logo/logo.svg'
-
+import Logo from "../../assets/Images/Logo/logo.svg";
 
 function LeftSide() {
   const percentage = 30;
@@ -12,7 +19,9 @@ function LeftSide() {
       <div className="flex flex-col gap-5 p-2">
         {/* Profile Info */}
         <div className="bg-white flex-between max-h-20 rounded-3xl p-2">
-          <span className="rounded-full text-slate-200 ring-4 ring-primary"><AccountCircle className="size-14"/></span>
+          <span className="rounded-full text-slate-200 ring-4 ring-primary">
+            <AccountCircle className="size-14" />
+          </span>
           <p className="flex flex-col gap-1">
             <span className="font-DanaBold">محمد قربانی</span>
             <span className="text-sm">09358923824</span>
@@ -22,38 +31,52 @@ function LeftSide() {
         <div className="flex flex-col items-center justify-between h-full bg-primary/10 p-1 rounded-3xl">
           {/* Top */}
           <div>
-          <h2 className="flex-center my-3 font-MorabbaBold text-base">
-            شارژ باقی مانده سیم کارت
-          </h2>
-          <div className="w-full mx-auto">
-            <CircularProgressbarWithChildren
-              value={percentage}
-              styles={buildStyles({
-                strokeLinecap: "butt",
-                textSize: "14px",
-                pathTransitionDuration: 0.001,
-                pathColor: `rgba(255, 204, 0)`,
-                textColor: "#000",
-                trailColor: "#d6d6d6",
-              })}
-            >
-        <p className="font-DanaBold text-xl">{percentage} تومان</p>
-            </CircularProgressbarWithChildren>
-          </div>
-          <p className="flex-center bg-primary font-DanaBold text-sm my-8 p-2 rounded-lg shadow-ring">سیم کارتت رو شارژ کن</p>
-          {/* Services */}
-          <p className="font-DanaBold my-8">خدمات فعال</p>
-             
-          <Services icon={<MailOutlineOutlined />} title="بسته پیامک هفته ای" date="تا تاریخ ۳۱ خرداد ۱۴۰۰" />
-          <Services icon={<WifiOutlined />} title="بسته  اینترنت ماهانه" date="تا تاریخ ۱۲ خرداد ۱۴۰۰" />
+            <h2 className="flex-center my-3 font-MorabbaBold text-base">
+              شارژ باقی مانده سیم کارت
+            </h2>
+            <div className="w-full mx-auto">
+              <CircularProgressbarWithChildren
+                value={percentage}
+                styles={buildStyles({
+                  strokeLinecap: "butt",
+                  textSize: "14px",
+                  pathTransitionDuration: 0.001,
+                  pathColor: `rgba(255, 204, 0)`,
+                  textColor: "#000",
+                  trailColor: "#d6d6d6",
+                })}
+              >
+                <p className="font-DanaBold text-xl">{percentage} تومان</p>
+              </CircularProgressbarWithChildren>
+            </div>
+            <p className="flex-center bg-primary/10 font-DanaBold text-sm my-8 p-2 rounded-lg border border-primary/30 relative">
+              <span class="absolute left-0 -top-1 flex size-3">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
+                <span class="relative inline-flex rounded-full size-3 bg-primary"></span>
+              </span>
+              سیم کارتت رو شارژ کن
+            </p>
+            {/* Services */}
+            <p className="font-DanaBold my-8">خدمات فعال</p>
+
+            <Services
+              icon={<MailOutlineOutlined />}
+              title="بسته پیامک هفته ای"
+              date="تا تاریخ ۳۱ خرداد ۱۴۰۰"
+            />
+            <Services
+              icon={<WifiOutlined />}
+              title="بسته  اینترنت ماهانه"
+              date="تا تاریخ ۱۲ خرداد ۱۴۰۰"
+            />
           </div>
           {/* Button Banner */}
-           <div className="w-full bg-white flex-between max-h-20 rounded-xl mb-4 p-2 font-MorabbaBold">
-                 <Link to="https://myket.ir/app/com.myirancell">
-                   دانلود اپلیکشین ما
-                 </Link>
-                 <img src={Logo} alt='Logo' className='size-14'/>
-           </div>
+          <div className="w-full bg-white flex-between max-h-20 rounded-xl mb-4 p-2 font-MorabbaBold">
+            <Link to="https://myket.ir/app/com.myirancell">
+              دانلود اپلیکشین ما
+            </Link>
+            <img src={Logo} alt="Logo" className="size-14 animate-pulse" />
+          </div>
         </div>
       </div>
     </section>
@@ -62,17 +85,18 @@ function LeftSide() {
 
 export default LeftSide;
 
-
-const Services = ({icon , title , date}) => {
-  return(
+const Services = ({ icon, title, date }) => {
+  return (
     <div className="flex-center gap-3 mb-6">
-        <p className="size-10 flex-center bg-primary/20 text-primary rounded-lg">{icon}</p>
-        <div className="flex flex-col gap-2.5 text-sm">
-           <span>{title}</span>
-           <span>{date}</span>
-        </div>
-    </div> 
-  )
-}
+      <p className="size-10 flex-center bg-primary/20 text-primary rounded-lg">
+        {icon}
+      </p>
+      <div className="flex flex-col gap-2.5 text-sm">
+        <span>{title}</span>
+        <span className="text-xs tracking-tightest">{date}</span>
+      </div>
+    </div>
+  );
+};
 
-export {Services}
+export { Services };
